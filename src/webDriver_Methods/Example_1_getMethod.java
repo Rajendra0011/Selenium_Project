ae1e1cf9f83f07a143d664c9cfef92937ac20cdc
rajendrakarhale0011@gmail.com <rajendrakarhale0011@gmail.com>
2025-06-04 12:58:53 +0530

    T1

---
+++ src/webDriver_Methods/Example_1_getMethod.java
@@ -0,0 +1,23 @@
+package webDriver_Methods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Example_1_getMethod {
+	
+	//WebDriver : WebDriver is an interface used to perform actions on Browser
+	
+	public static void main(String  args[]) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Automations Skills\\Browser Driver\\chromedriver.exe");
+		
+		WebDriver driver= new ChromeDriver();
+		
+//		get() : method is used to enter URL in browser
+		
+		driver.get("https://www.selenium.com");
+		
+		driver.manage().window().maximize();
+	}
+
+}
